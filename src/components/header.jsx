607e3aa7b623623d8d5531f373e5cb3be719d24b
k-alm <kauan.alm.almeida@gmail.com>
2023-08-28 12:05:36 -0300

    Iniciando com React

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+    return(
+        <>
+        <header>
+        <h1></h1>
+        <ul>
+            <li><a href="#">Item - 1</a></li>
+            <li><a href="#">Item - 2</a></li>
+            <li><a href="#">Item - 3</a></li>
+        </ul>
+        </header>
+        </>
+    )
+}
