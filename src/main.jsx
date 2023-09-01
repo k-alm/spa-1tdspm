c95fb2b7f6a6e444a4897cefeb347664e76e1d27
k-alm <kauan.alm.almeida@gmail.com>
2023-09-01 09:05:15 -0300

    Iniciando a construção de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+// Importando as páginas
+import Home from './routes/Home.jsx'
+import Produtos from './routes/Produto.jsx'
+import Erro404 from './routes/Erro404.jsx'
+import EditarProdutos from './routes/Produto.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/routes/EditarProdutos.jsx
@@ -0,0 +1,5 @@
+export default function EditarProdutos(){
+    return(
+        <div>Editar Produtos</div>
+    )
+}
