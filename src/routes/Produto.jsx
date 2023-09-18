d7695e87378cca9169a9afe5ecfc7535208ca70d
k-alm <kauan.alm.almeida@gmail.com>
2023-09-18 12:08:11 -0300

    usando o useEffects

--- src/routes/Produto.jsx
+++ src/routes/Produto.jsx
@@ -3,14 +3,28 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import {GrFormEdit as Editar} from "react-icons/gr";
 import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
 import style from "./Produtos.module.css";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
   document.title = "Produtos";
 
+  const [counter, setCounter] = useState(0);
+  const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
+
+  useEffect(() =>{
+    console.log("Esse effect renderiza sempre.")
+  })
+
+  useEffect(() => {
+    setListaProdutosLocal(ListaProdutos);
+  })
+
   return ( 
     <div>
       <h1>LISTA DE PRODUTOS</h1>
-
+      <div>
+        <button onClick={() => setCounter(counter + 1)}>COUNTER - {counter}</button>
+      </div>
       <table className={style.tblEstilo}>
         <thead>
         <tr>
@@ -23,7 +37,7 @@ export default function Produtos() {
         </thead>
         <tbody>
         {
-          ListaProdutos.map((item,indice)=>(
+          listaProdutosLocal.map((item,indice)=>(
             <tr key={indice} className={style.tblLine}>
                 <td>{item.id}</td>
                 <td>{item.nome}</td>
