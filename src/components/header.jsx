e25f2d4c07b761fc1f70668143206e21fef252b5
k-alm <kauan.alm.almeida@gmail.com>
2023-09-22 09:44:23 -0300

    Cadastrar Produtos feito

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,7 +10,7 @@ export default function Header(){
                 <li><Link to={"/"}><H></H></Link></li>
                 <li><Link to={"/produtos"}>Produtos</Link></li>
                 <li><Link to={"/editar/produtos/1"}>Editar produtos</Link></li>
-                <li><Link to={"/produtos"}>CADASTRAR PRODUTO</Link></li>
+                <li><Link to={"/cadastrar/produto"}>CADASTRAR PRODUTO</Link></li>
             </ul>
             </header>
         </>
--- src/routes/InserirProduto.jsx
+++ src/routes/InserirProduto.jsx
@@ -1,22 +1,63 @@
+import { useState } from "react";
 import { ListaProdutos } from "../components/ListaProdutos";
+import { useNavigate } from "react-router-dom";
 
 export default function InserirProduto() {
     document.title = "CADASTRAR";
 
-    //Gerando o id
-    let novoId = (ListaProdutos[ListaProdutos.length - 1].id + 1)
+    const navigate = useNavigate();
+
+    //Gerando um novo ID
+    let novoId = (ListaProdutos[ListaProdutos.length - 1].id + 1);
 
     const [produto, setProduto] = useState({
-        id: novoId,
+        id:novoId,
         nome:"",
         desc:"",
         valor:""
     });
 
-    
+    const handeChange = (e) => {
+        e.preventDefault();
+        //Destructuring
+        const {name, value} = e.target;
+        //Através da função set do useState, vamos adicionar o valor(value) na propriedade name que é a mesma que foi declarada no useState produto.
+        //Utilize o SPREAD para tornar a função mais simples!!
+        setProduto({...produto, [name]:value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        ListaProdutos.push(produto);
+        navigate("/produtos");
+    }
+
   return (
     <div>
         <h1>Cadastro de Produtos</h1>
+        
+        <div>
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <legend>PRODUTO</legend>
+              <div>
+                <label htmlFor="idNome">Nome do Produto:</label>
+                <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handeChange}/>
+              </div>
+              <div>
+                <label htmlFor="idDesc">Descrição do Produto:</label>
+                <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handeChange}/>
+              </div>
+              <div>
+                <label htmlFor="idValor">Valor do Produto:</label>
+                <input type="text" name="valor" id="idValor" value={produto.valor} onChange={handeChange}/>
+              </div>
+              <div>
+                <button>CADASTRAR PRODUTO</button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
     </div>
   )
 }
