ea7b2bd8d42b8f5ff2ce5cd3434f611ed0c75a07
k-alm <kauan.alm.almeida@gmail.com>
2023-09-25 12:11:42 -0300

    Listagem dinâmica com API externa concluída

--- src/routes/Produto.jsx
+++ src/routes/Produto.jsx
@@ -3,9 +3,19 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import { GrFormEdit as Editar } from "react-icons/gr";
 import { RiDeleteBin2Fill as Excluir } from "react-icons/ri";
 import style from "./Produtos.module.css";
+import { useEffect } from "react";
 
 export default function Produtos() {
   document.title = "Produtos";
+
+  const [listaProdutoExterna, setListaProdutoExterna] = useState([{}])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/produtos", {method:"GET", headers:{"Content-Type":"application/json"}})
+    .then((response) => response.json())
+    .then((data) => {setListaProdutoExterna(data)})
+    .catch((error) => console.log(error))
+  }, [])
 
   return (
     <div>
@@ -22,7 +32,7 @@ export default function Produtos() {
         </thead>
         <tbody>
           {
-            ListaProdutos.map((item, indice) => (
+            listaProdutoExterna.map((item, indice) => (
               <tr key={indice} className={style.tblLine}>
                 <td>{item.id}</td>
                 <td>{item.nome}</td>
