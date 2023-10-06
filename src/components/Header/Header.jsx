53c2a3b8b16bf06e1b28c2453b6c0e700a5e3334
k-alm <kauan.alm.almeida@gmail.com>
2023-10-06 10:25:11 -0300

    update do cabeçalho

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {AiFillHome as H} from "react-icons/ai";
 import "./Header.scss";
 
 export default function Header(){
+
+    const rotaAtual = useLocation();
+
+
+    
     return(
         <>
             <header className="cabecalho">
             <ul>
-                <li><Link to={"/"}><H></H></Link></li>
-                <li><Link to={"/produtos"}>PRODUTOS</Link></li>
-                <li><Link to={"/cadastrar/produto"}>CADASTRAR PRODUTO</Link></li>
+                <li><Link to={"/"} className={rotaAtual.pathname == "/" ? "active" : ""}><H></H></Link></li>
+                <li><Link to={"/produtos"} className={rotaAtual.pathname == "/produtos" ? "active" : ""}>PRODUTOS</Link></li>
+                <li><Link to={"/cadastrar/produto"} className={rotaAtual.pathname == "/cadastrar/produto" ? "active" : ""}>CADASTRAR PRODUTO</Link></li>
             </ul>
             </header>
         </>
